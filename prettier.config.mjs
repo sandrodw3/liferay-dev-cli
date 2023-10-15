0d524186bf1b1a9b25a056a1d73cffa2e0f9f004
Sandro Chinea <sandrochinea@gmail.com>
2023-10-15 12:52:00 +0100

    Add prettier configuration

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+	bracketSpacing: true,
+	quoteProps: 'consistent',
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+}
+
+export default config
