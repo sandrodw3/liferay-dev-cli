ccbcbea8601ef81af02ac35f8d1df2cf05301622
Sandro Chinea <sandrochinea@gmail.com>
2023-10-15 13:08:19 +0100

    Export liferay functions

---
+++ src/shared/liferay/index.ts
@@ -0,0 +1,10 @@
+export type { Profile } from './liferay-profiles.ts'
+
+export { CMDS } from './cmds.ts'
+export { PROFILES } from './liferay-profiles.ts'
+export { ROOT_MODULES } from './root-modules.ts'
+
+export { getChangedModules } from './get-changed-modules.ts'
+export { getModules } from './get-modules.ts'
+export { getParentModule } from './get-parent-module.ts'
+export { isLiferayModule } from './is-liferay-module.ts'
