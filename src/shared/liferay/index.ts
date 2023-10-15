519a23e16fc4afa6ec557f0c72d2489f24a79972
Sandro Chinea <sandrochinea@gmail.com>
2023-10-15 13:08:19 +0100

    Export liferay functions

---
+++ src/shared/liferay/index.ts
@@ -0,0 +1,10 @@
+export type { ModuleType } from './get-module-type.ts'
+export type { Profile } from './liferay-profiles.ts'
+
+export { PROFILES } from './liferay-profiles.ts'
+export { ROOT_MODULES } from './root-modules.ts'
+
+export { getChangedModules } from './get-changed-modules.ts'
+export { getLatestTomcatPath } from './get-latest-tomcat-path.ts'
+export { getModuleType } from './get-module-type.ts'
+export { selectModule } from './select-module.ts'
