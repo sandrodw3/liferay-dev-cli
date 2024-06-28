7742e48d460d7543af713149c0833ae6649c1cf0
Sandro Chinea <sandrochinea@gmail.com>
2024-06-28 10:46:37 +0100

    Export CLI configuration utilities

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export type { ConfigEntry } from './config-entry.ts'
+
+export { CONFIG_ENTRY_KEYS } from './config-entry.ts'
+export { CONFIG_PATH } from './config-path.ts'
+
+export { getConfigEntry } from './get-config-entry.ts'
+export { getConfig } from './get-config.ts'
+export { initConfig } from './init-config.ts'
+export { setConfigEntry } from './set-config-entry.ts'
