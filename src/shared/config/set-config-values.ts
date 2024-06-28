7cc92e7d8c0176db6b1926dc5cca64acee92f183
Sandro Chinea <sandrochinea@gmail.com>
2024-06-28 10:43:43 +0100

    Add command to manage app configuration

--- src/shared/config/set-config-values.ts
+++ src/shared/config/set-config-values.ts
@@ -1,27 +1,47 @@
-import { Input, Secret, prompt } from 'cliffy/prompt'
+import { Confirm, Input, Secret, prompt } from 'cliffy/prompt'
 import { bold, red, white, yellow } from 'std/colors'
 import { existsSync } from 'std/exists'
 
-import { readConfig, writeConfig } from 'config'
+import {
+	CONFIG_PATH,
+	ConfigEntry,
+	getConfigEntry,
+	readConfig,
+	writeConfig,
+} from 'config'
 import { Client, configLogger } from 'mysql'
-import { join, log } from 'tools'
-import { CONFIG_PATH } from './config.ts'
+import { checkDbExists, createDb, join, log } from 'tools'
 
 /**
  * Ask user for config and store it in .lfr/config file
  */
 
-export async function setConfigValues() {
-	const config = await readConfig()
+export async function setConfigValues(entries?: ConfigEntry[]) {
+	let config
 
-	if (!config) {
-		log(`Introduce data to ${bold(white('initialize'))} config\n`)
-	} else {
+	try {
+		config = await readConfig()
+	} catch {
+		config = {}
+	}
+
+	if (!config['portal.path']) {
+		log(
+			`Introduce data to ${bold(white('initialize'))} config. It will be ${bold(white('saved'))} in ${CONFIG_PATH}\n`
+		)
+	} else if (!entries?.length) {
 		log(`Introduce ${bold(white('new'))} config values\n`)
 	}
 
 	const answers = await prompt([
 		{
+			before: async (_, next) => {
+				if (entries?.length && !entries.includes('portal.path')) {
+					await next(true)
+				} else {
+					await next()
+				}
+			},
 			default: config?.['portal.path'],
 			name: 'portal.path',
 			message: 'Liferay portal path',
@@ -37,78 +57,129 @@ export async function setConfigValues() {
 			},
 		},
 		{
+			before: async (_, next) => {
+				if (entries?.length && !entries.includes('base.branch')) {
+					await next(true)
+				} else {
+					await next()
+				}
+			},
 			default: config?.['base.branch'] || 'master',
 			name: 'base.branch',
 			message: 'Base branch',
 			type: Input,
-			after: async (_, next) => {
-				log(
-					`\n${bold('MySQL')} data (${bold(
-						yellow('Optional')
-					)}, press ENTER to skip)\n`
-				)
-
-				await next()
-			},
 		},
 		{
+			before: async (_, next) => {
+				if (entries?.length && !entries.includes('mysql.user')) {
+					await next(true)
+				} else {
+					await next()
+				}
+			},
 			default: config?.['mysql.user'],
 			name: 'mysql.user',
 			message: 'MySQL User',
+			minLength: 1,
 			type: Input,
-			after: async (values, next) => {
-				if (values['mysql.user']) {
-					await next()
-				} else {
-					delete values['mysql.user']
-					delete values['mysql.pw']
-					delete values['mysql.db']
-				}
-			},
 		},
 		{
+			before: async (_, next) => {
+				if (entries?.length && !entries.includes('mysql.pw')) {
+					await next(true)
+				} else {
+					await next()
+				}
+			},
 			default: config?.['mysql.pw'],
 			name: 'mysql.pw',
 			message: 'MySQL Password',
+			minLength: 1,
 			type: Secret,
-		},
-		{
-			default: config?.['mysql.db'],
-			name: 'mysql.db',
-			message: 'Liferay portal database name',
-			type: Input,
 			after: async (answers, next) => {
 				// Check connection is valid
 
 				try {
 					const client = await new Client().connect({
-						db: answers['mysql.db'],
 						username: answers['mysql.user'],
 						password: answers['mysql.pw'],
 					})
 
 					configLogger({ enable: false })
 
-					await client.execute(`SHOW TABLES`)
+					await client.execute(`SHOW DATABASES`)
+
+					await next()
 				} catch {
 					log(
 						`\n${bold(
 							red('Could not connect')
-						)} with these credentials, please try again (press ENTER to skip)\n`
+						)} with these credentials, please try again\n`
 					)
 
 					await next('mysql.user')
 				}
 			},
 		},
+		{
+			before: async (_, next) => {
+				if (entries?.length && !entries.includes('mysql.db')) {
+					await next(true)
+				} else {
+					await next()
+				}
+			},
+			default: config?.['mysql.db'],
+			name: 'mysql.db',
+			message: 'Liferay portal database name',
+			minLength: 1,
+			type: Input,
+			after: async (answers, next) => {
+				const username =
+					answers['mysql.user'] ||
+					(await getConfigEntry('mysql.user'))
+
+				const password =
+					answers['mysql.pw'] || (await getConfigEntry('mysql.pw'))
+
+				const exists = await checkDbExists({
+					username,
+					password,
+					database: answers['mysql.db']!,
+				})
+
+				if (exists) {
+					await next()
+
+					return
+				}
+
+				if (
+					await Confirm.prompt(
+						`The database ${bold(yellow('does not exist'))}, do you want to create it?`
+					)
+				) {
+					await createDb({
+						username,
+						password,
+						database: answers['mysql.db']!,
+					})
+				} else {
+					Deno.exit()
+				}
+			},
+		},
 	])
 
 	try {
-		await writeConfig(answers)
+		const nextConfig = { ...config, ...answers }
+
+		await writeConfig(nextConfig)
 
 		log(
 			`\nConfiguration was ${bold(white('correctly saved'))} in ${CONFIG_PATH}`
 		)
+		return nextConfig
 	} catch {
 		log(`\nConfiguration ${red('could not be saved')}, please try again`)
 
