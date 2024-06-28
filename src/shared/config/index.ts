92ed3acfd007ca84b8c8e10ecf39a43f3d7e02b1
Sandro Chinea <sandrochinea@gmail.com>
2024-06-28 10:46:37 +0100

    Export configuration utilities

---
+++ src/shared/config/index.ts
@@ -0,0 +1,7 @@
+export type { ConfigEntry } from './config-entry.ts'
+
+export { CONFIG_ENTRY_KEYS } from './config-entry.ts'
+
+export { getConfigEntry, setConfigEntry } from './config-entry.ts'
+export { readConfig, writeConfig } from './config.ts'
+export { initConfig } from './init-config.ts'
