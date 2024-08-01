fb051ae72dab106e514891f9f023f35069bf86d2
Sandro Chinea <sandrochinea@gmail.com>
2024-08-01 23:48:48 +0100

    Export version utilities

---
+++ src/shared/version/index.ts
@@ -0,0 +1,3 @@
+export { checkUpdate } from './check-update.ts'
+export { getLatestVersion } from './get-latest-version.ts'
+export { getLastCheckDate, setLastCheckDate } from './last-check-date.ts'
