a2d942e2537ba524ba8eecf890d23abb1f2d3c61
Sandro Chinea <sandrochinea@gmail.com>
2024-09-11 18:56:32 +0100

    Add function to get the type of a liferay module

---
+++ src/shared/liferay/get-module-type.ts
@@ -0,0 +1,62 @@
+import { getConfigEntry } from 'config'
+import { ROOT_MODULES } from 'liferay'
+import { existsSync } from 'std/exists'
+import { getBaseName, join } from 'tools'
+
+export type ModuleType =
+	| 'standard-module'
+	| 'parent-module'
+	| 'test-module'
+	| 'root'
+	| 'root-module'
+	| 'playwright'
+
+/**
+ * Return type of the given module
+ */
+
+export async function getModuleType(
+	module: string
+): Promise<ModuleType | null> {
+	const portalPath = await getConfigEntry('portal.path')
+
+	if (module === portalPath) {
+		return 'root'
+	} else if (isRootModule(module)) {
+		return 'root-module'
+	} else if (module.endsWith('test/playwright')) {
+		return 'playwright'
+	} else if (existsSync(join(module, 'bnd.bnd')) && module.endsWith('test')) {
+		return 'test-module'
+	} else if (isParentModule(module)) {
+		return 'parent-module'
+	} else if (existsSync(join(module, 'bnd.bnd'))) {
+		return 'standard-module'
+	}
+
+	return null
+}
+
+/**
+ * Check whether a module is a parent module or not
+ */
+
+function isParentModule(module: string) {
+	const moduleName = getBaseName(module)
+
+	for (const entry of Deno.readDirSync(module)) {
+		if (entry.name.startsWith(`${moduleName}-`) && !entry.isFile) {
+			return true
+		}
+	}
+
+	return false
+}
+
+/**
+ * Check whether a module is a root module or not
+ */
+
+function isRootModule(module: string) {
+	return ROOT_MODULES.some((rootModule) => module.includes(rootModule))
+}
