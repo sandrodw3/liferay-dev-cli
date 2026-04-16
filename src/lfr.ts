d8ceb301261016f06319c894ad93d09569e74726
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-04-16 16:01:09 +0200

    Update to v1.8.0

--- src/lfr.ts
+++ src/lfr.ts
@@ -28,7 +28,7 @@ import { checkUpdate, getLastCheckDate } from '@lib/version'
 
 // Create command
 
-const VERSION = 'v1.7.0'
+const VERSION = 'v1.8.0'
 
 const command = new Command()
 	.name('lfr')
