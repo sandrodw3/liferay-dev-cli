4984e1befac008865ab0e6cf7baf15943236034d
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-04-28 20:43:42 +0100

    Compile lfr on install

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 deno install \
-  --allow-env \
-  --allow-net \
-  --allow-read \
-  --allow-run \
-  --allow-write \
-  --import-map https://raw.githubusercontent.com/sandrodw3/liferay-dev-cli/main/deno.json \
-  https://raw.githubusercontent.com/sandrodw3/liferay-dev-cli/main/src/lfr.ts \
-  -f -g -r
+	--allow-env \
+	--allow-net \
+	--allow-read \
+	--allow-run \
+	--allow-write \
+	--compile \
+	--import-map https://raw.githubusercontent.com/sandrodw3/liferay-dev-cli/main/deno.json \
+	https://raw.githubusercontent.com/sandrodw3/liferay-dev-cli/main/src/lfr.ts \
+	-f -g -r
